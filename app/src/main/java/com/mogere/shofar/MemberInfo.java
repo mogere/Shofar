8500f3a6a486cb91848b40efcaf25f205717df9d
mogere <pmnyatichi@gmail.com>
2020-02-02 22:06:54 +0300

    receive member info in member info class

--- app/src/main/java/com/mogere/shofar/MemberInfo.java
+++ app/src/main/java/com/mogere/shofar/MemberInfo.java
@@ -2,6 +2,7 @@ package com.mogere.shofar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MemberInfo extends AppCompatActivity {
@@ -10,5 +11,8 @@ public class MemberInfo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_member_info);
+
+        Intent intent = getIntent();
+        String memberName = intent.getStringExtra("member");
     }
 }
