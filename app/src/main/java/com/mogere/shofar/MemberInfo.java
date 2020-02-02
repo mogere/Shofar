0b5cb84937ccf814a69c75c8e8be8374c0301fdb
mogere <pmnyatichi@gmail.com>
2020-02-02 22:12:20 +0300

    display member name in member textview

--- app/src/main/java/com/mogere/shofar/MemberInfo.java
+++ app/src/main/java/com/mogere/shofar/MemberInfo.java
@@ -4,15 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MemberInfo extends AppCompatActivity {
 
+    @BindView(R.id.name) TextView mName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_member_info);
 
+        ButterKnife.bind(this);
         Intent intent = getIntent();
         String memberName = intent.getStringExtra("member");
+        mName.setText(memberName);
     }
 }
